5bcea5d1080560ce72876229ac74f3669844b103
billxu <billxu0521@gmail.com>
2024-10-08 09:36:50 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* POST webhooks */
+router.post('/webhooks', function(req, res, next) {
+  // 立即发送200 OK响应
+  res.status(200).send('OK');
+
+  // 异步处理webhook请求
+  setImmediate(() => {
+    console.log('收到 webhook 请求:', req.body);
+    // 在这里添加其他处理逻辑
+  });
+});
+
 module.exports = router;
